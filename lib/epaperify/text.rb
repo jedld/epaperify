684f73dfca07a3c26119ad64a916708b62be9152
Joseph Emmanuel Dayo <joseph.dayo@cheetahdigital.com>
2021-10-12 17:01:15 +0800

    text measure wip

--- lib/epaperify/text.rb
+++ lib/epaperify/text.rb
@@ -4,13 +4,55 @@ module Epaperify
       @font = font
       @font_cache = {}
       @line_padding = 5
+      @string = ""
+
+      # pre render space
+
+      @font_cache[' '.ord] ||= begin
+        @font.render_font(' '.ord)
+      end
+
+      @space_width = @font_cache[' '.ord].advance_width
     end
 
     def line_padding(padding)
       @line_padding = padding
     end
 
-    def render_string(canvas, str, options = {})
+    def string=(str)
+      @string = str
+
+      str.each_char do |c|
+        @font_cache[c.ord] ||= begin
+          @font.render_font(c.ord)
+        end
+      end
+    end
+
+    def string
+      @string
+    end
+
+    def measure(options = {})
+      render(0, 0, canvas, options)
+    end
+
+    def render_string(canvas, options = {})
+      x = options.fetch(:x, canvas.x_cursor)
+      y = options.fetch(:y, canvas.y_cursor)
+
+      canvas.x_cursor, canvas.y_cursor = render(x, y, canvas, options) do |x_point, y_point, font_render|
+        canvas.render_font_buffer(x_point + font_render.leftside_bearing, y_point + font_render.yoffset, font_render, 0)
+      end
+    end
+
+    private
+
+    def is_word_char?(c)
+      !["\n", "\r", " "].include?(c)
+    end
+
+    def render(x, y, canvas, options = {}, &block)
       max_width = options.fetch(:width, canvas.width)
       left_margin = options.fetch(:left_margin, 0)
       right_margin = options.fetch(:right_margin, 0)
@@ -18,25 +60,14 @@ module Epaperify
 
       y_advance = (@font.ascender + @font.descender + @font.linegap)
 
-      x_point = canvas.x_cursor + left_margin
-      y_point = canvas.y_cursor + y_advance + top_margin
-      
-      @font_cache[' '.ord] ||= begin
-                                 @font.render_font(' '.ord)
-                               end
-      space_width = @font_cache[' '.ord].advance_width
-
-      str.each_char do |c|
-        @font_cache[c.ord] ||= begin
-          @font.render_font(c.ord)
-        end
-      end
+      x_point = + left_margin
+      y_point = options.fetch(:y, canvas.y_cursor) + y_advance + top_margin
 
       inside_word = false
       word_boundaries = {}
       current_boundary = nil
 
-      str.each_char.with_index do |c, index|
+      @string.each_char.with_index do |c, index|
         if is_word_char?(c)
           font_render = @font_cache[c.ord]
           if !inside_word
@@ -57,14 +88,12 @@ module Epaperify
         end
       end
 
-      
-
-      str.each_char.with_index do |c, index|
+      @string.each_char.with_index do |c, index|
         if c == "\n"
           x_point = left_margin
           y_point +=  y_advance + @line_padding
         elsif c == "\t"
-          x_point += 4 * space_width 
+          x_point += 4 * @space_width
         else
           if word_boundaries.key?(index) && max_width
             if x_point + word_boundaries.dig(index, :width) > max_width - right_margin
@@ -74,18 +103,16 @@ module Epaperify
           end
 
           font_render = @font_cache[c.ord]
-          canvas.render_font_buffer(x_point + font_render.leftside_bearing, y_point + font_render.yoffset, font_render, 0)
+
+          if block_given?
+            yield x_point, y_point, font_render
+          end
+
           x_point += font_render.advance_width
         end
       end
-      canvas.x_cursor = x_point
-      canvas.y_cursor = y_point
-    end
 
-    private
-
-    def is_word_char?(c)
-      !["\n", "\r", " "].include?(c)
+      [x_point, y_point]
     end
 
   end
