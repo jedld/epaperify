3b92bea9211f0235f1883c05da3ae5a23169b21c
Joseph Emmanuel Dayo <joseph.dayo@cheetahdigital.com>
2021-10-20 18:07:20 +0800

    Update sample font

--- sample/sample_font.rb
+++ sample/sample_font.rb
@@ -2,12 +2,9 @@ require "bundler/setup"
 require "epaperify"
 require 'pry-byebug'
 
-canvas = Epaperify::PaperCanvas.new(Epaperify::Devices::EPD_IT8951, 270, 2270)
-
-_font = Epaperify::Font.new(12, "FiraGO-Regular.ttf")
+canvas = Epaperify::PaperCanvas.new(Epaperify::Devices::EPD_IT8951, 0, 2270)
+puts "canvas size #{canvas.width}, #{canvas.height}"
 font2 = Epaperify::Font.new(12, "orange juice 2.0.ttf")
-
-font3 = Epaperify::Font.new(12, "Admiration Pains.ttf")
 text = Epaperify::Text.new(font2)
 text.string = "Hello World!\nI hope everyone is safe!\n"
 width, height = text.measure canvas
